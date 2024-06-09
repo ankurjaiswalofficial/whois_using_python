77ad1d0df4afb78605b7115e7c2c3dd2b1123fd0
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2024-06-09 09:12:08 +0530

    class based URL Handler

--- urlHandler.py
+++ urlHandler.py
@@ -1,16 +1,34 @@
 from tld import get_tld
 
-def extract_domain_info(link):
-    domain = get_tld(link, as_object=True, fail_silently=True)
-    subdomain = domain.subdomain if domain.subdomain else None
-    domain_name = domain.fld
-    domain_extension = domain.tld
+class UrlHandler:
+    def __init__(self, url) -> dict:
+        self.url = url
+        self.subdomain = ""
+        self.domain_name = ""
+        self.domain = ""
+        self.domain_extension = ""
+        self.domain_data = None
+        
+        self.extract_domain_info()
+        self.get_dict_data()
 
-    return subdomain, domain_name, domain_extension
+    def extract_domain_info(self):
+        extracted_data = get_tld(self.url, as_object=True, fail_silently=True)
+        self.domain_data = extracted_data
 
-# Example usage:
-link = "http://some..hello.ibn.subdomain.google.co.uk"
-subdomain, domain, domain_extension = extract_domain_info(link)
-print("Subdomain:", subdomain)
-print("Domain:", domain)
-print("Domain Extension:", domain_extension)
+    def get_dict_data(self):
+        self.subdomain = self.domain_data.subdomain if self.domain_data.subdomain else None
+        self.domain_name = self.domain_data.fld
+        self.domain_extension = self.domain_data.tld
+        self.domain = self.domain_name.replace("." + self.domain_extension, "")
+        return {
+            "subdomain": self.subdomain,
+            "domain_name": self.domain_name,
+            "domain": self.domain,
+            "domain_extension": self.domain_extension
+        }
+    def __repr__(self):
+        return str(self.get_dict_data())
+
+url_handler = UrlHandler("http://rajannedfgh.sdgjh.co.in")
+print(url_handler)
